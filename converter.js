21a290a10d3819e5f8a8d6e2bf51b25990816624
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2019-11-27 23:28:09 -0300

    Melhorias na automação do script

--- converter.js
+++ converter.js
@@ -1,26 +1,40 @@
 var fs = require('fs');
 
-const bLanguage = 'ar';
-const bVersion = 'SVD';
-const arrayWay = ['books','JSON'];
+const arrayJSON = fs.readdirSync('./bible/JSON');
 
-let way = `.`;
-const arrayBook =[];
-const jsCall = [];
-const jsonFile = bLanguage+bVersion;
+const autoGetJSON = (array) =>{
+    const result = array.map(e => {
 
-arrayWay.push(bLanguage);
-arrayWay.push(bVersion);
+        e = e.split('');
 
-const getJSON = require(`./bible/JSON/${jsonFile}.json`);
+        let language = '';
+        let version = '';
+        let test = 'false';
+        e.forEach( (e, index) => {            
 
-for(let a = 0; a < arrayWay.length; a++){
-    way += `/${arrayWay[a]}`
-    
-    if(!fs.existsSync(way)){
-        fs.mkdirSync(way)
-    }
+            if(e.charCodeAt(0)>=65 && e.charCodeAt(0)<=90){
+               test = 'true' 
+            }else if(e == '.'){
+                test = 'ponto';
+            }
+
+
+            if(test == 'true'){
+                version += e;
+            }else if (test == 'false'){
+                language += e;
+            }
+
+        })
+
+        return [language, version];
+    })
+
+    return result
 }
+
+const arrayLanguageVersion = autoGetJSON(arrayJSON);
+
 
 const assentRemove = (text) =>{       
     text = text.toString().toLowerCase();                                                         
@@ -33,75 +47,100 @@ const assentRemove = (text) =>{
     return text.replace(' ','').replace(' ', '').replace('1','I').replace('2','II').replace('3','III');                 
 };
 
-for (let w =0; w < getJSON.length; w++){
+for(let g = 0; g < arrayJSON.length; g++){
     
-    let print = getJSON[w]
-    print = print.chapters.map(e => e)
+    const bLanguage = arrayLanguageVersion[g][0];
+    const bVersion = arrayLanguageVersion[g][1];
+    const arrayWay = ['books','JSON'];
 
-    const obj = new Object;
-    const name = getJSON[w].name
-    const book = assentRemove(name)
+    let way = `.`;
+    const arrayBook =[];
+    const jsCall = [];
+    const jsonFile = bLanguage+bVersion;
 
-    arrayBook.push(book);
+    arrayWay.push(bLanguage);
+    arrayWay.push(bVersion);
 
-    obj.language = bLanguage
-    obj.version = getJSON.verson
-    obj.title = name;
-    obj.abbreviation = assentRemove(getJSON[w].abbrev);
+    const getJSON = require(`./bible/JSON/${jsonFile}.json`);
 
-    if(w < 39){
-        obj.newTestament = false;
-    }else{
-        obj.newTestament = true;
-
+    for(let a = 0; a < arrayWay.length; a++){
+        way += `/${arrayWay[a]}`
+        
+        if(!fs.existsSync(way)){
+            fs.mkdirSync(way)
+        }
     }
 
+    for (let w =0; w < getJSON.length; w++){
+        
+        let print = getJSON[w]
+        print = print.chapters.map(e => e)
 
-    const array = value => {
-        const fullArray = [];
+        const obj = new Object;
+        const name = getJSON[w].name
+        const book = assentRemove(name)
 
-        for(let i = 0; i < print.length; i++){
-            const print2 = value[i].map(e => e)
-            fullArray[i] = print2;
-            obj[i+1] = {};
-            
-            print2.forEach((element, index) => {
-                obj[i+1][index+1] = element    
-            });
+        arrayBook.push(book);
+
+        obj.language = bLanguage
+        obj.version = getJSON.verson
+        obj.title = name;
+        obj.abbreviation = assentRemove(getJSON[w].abbrev);
+
+        if(w < 39){
+            obj.newTestament = false;
+        }else{
+            obj.newTestament = true;
 
         }
-        return obj;
+
+
+        const array = value => {
+            const fullArray = [];
+
+            for(let i = 0; i < print.length; i++){
+                const print2 = value[i].map(e => e)
+                fullArray[i] = print2;
+                obj[i+1] = {};
+                
+                print2.forEach((element, index) => {
+                    obj[i+1][index+1] = element    
+                });
+
+            }
+            return obj;
+        }
+
+        array(print)
+
+        jsCall.push(`${book} = require(\`./_${book}.json\`);`)
+
+
+        fs.writeFile(`${way}/_${book}.json`,`${JSON.stringify(obj)}`, function(err) {
+            if(err) {
+                console.log(err);
+            }
+        });
     }
 
-    array(print)
+    fs.writeFile(`${way}/index.js`,`module.exports = () => {
 
-    jsCall.push(`${book} = require(\`./_${book}.json\`);`)
+        ${jsCall.join(`
+        `)} 
 
+        return {
 
-    fs.writeFile(`${way}/_${book}.json`,`${JSON.stringify(obj)}`, function(err) {
+            ${arrayBook}
+
+        }
+
+    }`, function(err) {
         if(err) {
             console.log(err);
         } else {
             console.log("The file was saved!");
         }
     });
+
+    console.log(`Foi gerada a Biblía no idioma ${bLanguage} e versão ${bVersion}`);
 }
-
-fs.writeFile(`${way}/index.js`,`module.exports = () => {
-
-    ${jsCall.join(`
-    `)} 
-
-    return {
-
-        ${arrayBook}
-
-    }
-
-}`, function(err) {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log("The file was saved!");
-    }
-});
